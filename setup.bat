e0f83c1ad80aaea05a5bebdff2df7a186d08999b
Zenoro <voronets.vladimir@yandex.ru>
2025-10-09 12:48:00 +0300

    removed instagram login

--- setup.bat
+++ setup.bat
@@ -2,13 +2,9 @@
 setlocal
 
 set /p bot_token="Enter telegram-bot API: "
-set /p inst_login="Enter Instagram login: "
-set /p inst_password="Enter Instagram password: "
 
 (
 echo BOT_TOKEN='%bot_token%'
-echo INST_LOGIN='%inst_login%'
-echo INST_PASSWORD='%inst_password%'
 ) > .env
 
 set /p create_venv="Create Python venv? [Y/n]: "
